868f4534d65dbc287522d61610bd6ac44a63720a
senthilarun <senthilarun@sparkout.com>
2019-02-28 13:14:53 +0530

    Initial Commit on 28-02-2019

---
+++ app/src/main/java/com/gks/sociallogin/ActivityTwitter.kt
@@ -0,0 +1,12 @@
+package com.gks.sociallogin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTwitter : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitter)
+    }
+}
